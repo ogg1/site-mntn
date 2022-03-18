3ca22b57b57d7f9d998072946f94bf77aa9f02f2
ogg1 <81367529+ogg1@users.noreply.github.com>
2022-03-18 12:35:58 +0200

    add files

---
+++ js/main.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const navBtn = document.querySelector('.nav-button');
+const mobileNav = document.querySelector('.mobile-nav');
+const body = document.body;
+
+navBtn.addEventListener('click', function(event) {
+    event.stopPropagation();
+    mobileNav.classList.toggle('mobile-nav-active');
+    navBtn.classList.toggle('nav-button-close');
+    body.classList.toggle('no-scroll');
+});
+
+window.addEventListener('click', function() {
+
+    if (body.classList.contains('no-scroll')) {
+        body.classList.toggle('no-scroll');
+        navBtn.classList.toggle('nav-button-close');
+        mobileNav.classList.toggle('mobile-nav-active');
+    }
+});
+
+mobileNav.addEventListener('click', function(event) {
+    event.stopPropagation();
+});
